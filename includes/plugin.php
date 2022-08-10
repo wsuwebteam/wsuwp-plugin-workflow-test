a72e40826fcb465f8897710751005f2ee9e0451e
Dan White <dan.white1@wsu.edu>
2022-08-10 15:13:00 -0700

    Create test plugin

---
+++ includes/plugin.php
@@ -0,0 +1,50 @@
+<?php namespace WSUWP\Plugin\Workflow_Test_2;
+
+class Plugin
+{
+
+
+    private static $version = '0.0.1';
+
+
+    public static function get( $property )
+    {
+
+        switch ( $property ) {
+
+        case 'version':
+            return self::$version;
+
+        case 'dir':
+            return plugin_dir_path(dirname(__FILE__));
+
+        case 'url':
+            return plugin_dir_url(dirname(__FILE__));
+
+        case 'template_dir':
+            return plugin_dir_path(dirname(__FILE__)) . '/templates';
+
+        default:
+            return '';
+
+        }
+
+    }
+
+    public static function init()
+    {
+
+        // include_once __DIR__ . '/assets.php';
+
+    }
+
+
+    public static function require_class( $class_name )
+    {
+
+        include_once self::get('dir') . '/classes/class-' . $class_name . '.php';
+
+    }
+}
+
+Plugin::init();
---
+++ wsuwp-plugin-workflow-test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WSUWP Plugin | Workflow Test
+ * Plugin URI: https://github.com/wsuwebteam/wsuwp-plugin-workflow-test
+ * Description: Describe the plugin
+ * Version:    1.0.0
+ * Requires PHP: 7.3
+ * Author: Washington State University, Dan White
+ * Author URI: https://web.wsu.edu/
+ * Text Domain: wsuwp-plugin-workflow-test
+ */
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Initiate plugin
+require_once __DIR__ . '/includes/plugin.php';
