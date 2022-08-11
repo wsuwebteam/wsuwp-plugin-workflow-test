72e8140e916010cb5855b13ca04fc81b0cfa8656
Dan White <dan.white1@wsu.edu>
2022-08-11 10:01:06 -0700

    Update branch

--- wsuwp-plugin-workflow-test.php
+++ wsuwp-plugin-workflow-test.php
@@ -3,7 +3,7 @@
  * Plugin Name: WSUWP Plugin | Workflow Test
  * Plugin URI: https://github.com/wsuwebteam/wsuwp-plugin-workflow-test
  * Description: Describe the plugin
- * Version:    1.0.0
+ * Version:    1.0.1
  * Requires PHP: 7.3
  * Author: Washington State University, Dan White
  * Author URI: https://web.wsu.edu/
