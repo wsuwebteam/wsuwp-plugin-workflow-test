c6b73723df2e20196ccfdec0343f97a20e88ff61
Dan White <dan.white1@wsu.edu>
2023-04-17 10:29:19 -0700

    Last one

--- assets/src/front-end.js
+++ assets/src/front-end.js
@@ -1,4 +1,4 @@
 console.log("Hello World from front-end.js");
 console.log("This is an best feature feature");
-console.log("And another awesome feature feature");
+console.log("And another best feature feature");
 console.log("One more best feature feature");
